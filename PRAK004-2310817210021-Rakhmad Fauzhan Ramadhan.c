21883a518d0c3e821d82afe9d8b831f536249d3a
ojanojan1453 <144782320+ojanojan1453@users.noreply.github.com>
2023-09-16 15:11:28 +0800

    Add files via upload

---
+++ PRAK004-2310817210021-Rakhmad Fauzhan Ramadhan.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    printf("##############################\n");
+    printf("#                            #\n");
+    printf("#  Rakhmad Fauzhan Ramadhan  #\n");
+    printf("#       2310817210021        #\n");
+    printf("#                            #\n");
+    printf("##############################\n");
+    
+    return 0;
+}
